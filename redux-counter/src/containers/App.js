9a9f8ede08b938acd36accb70eb747b2240b39bd
leoh7 <leoh7@naver.com>
2019-01-21 03:00:55 +0900

    13.2.5.2 onCreate와 onRemove 만들고 Buttons 컴포넌트의 props로 전달

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-import CounterContainer from './CounterContainer';
+import React, { Component } from 'react';
+import Buttons from '../components/Buttons';
+import CounterListContainer from './CounterListContainer';
+import getRandomColor from '../lib/getRandomColor';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 class App extends Component {
   render() {
+    const { onCreate, onRemove } = this.props;
     return (
-      <div>
-        <CounterContainer/>
-      </div>
+        <div className="App">
+          <Buttons
+            onCreate={onCreate}
+            onRemove={onRemove}/>
+          <CounterListContainer/>
+        </div>
     )
   }
 }
-export default App;
+// 액션 생성 함수 준비
+const mapToDispatch = (dispatch) => ({
+  onCreate: () => dispatch(actions.create(getRandomColor())),
+  onRemove: () => dispatch(actions.remove())
+})
+
+// 리덕스에 연결시키고 내보냄
+export default connect(null, mapToDispatch)(App);
