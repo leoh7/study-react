a8fb02606bc52f01a81ccb7a8a31e2fb5383c2a0
leoh7 <leoh7@naver.com>
2019-01-21 02:06:17 +0900

    액션 생성 함수 수정 => 특정 카운터를 조정할 수 있도록 index 포함

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -3,15 +3,31 @@
 
 import * as types from './ActionTypes';
 
-export const increment = () => ({
-  type: types.INCREMENT
+// 새로 만들 때 기본 색상을 받을 수 있도록 파라미터로 설정
+export const create = (color) => ({
+  type: types.CREATE,
+  color
+})
+
+// 맨 마지막 카운터를 삭제하기 떄문에 index 필요 없음
+export const remove = () => ({
+  type: types.REMOVE
+})
+
+// 액션이 특정 카운터를 조작할 수 있도록 설정해야 해서 
+// index 값을 액션 객체에 포함함
+export const increment = (index) => ({
+  type: types.INCREMENT,
+  index
 });
 
-export const decrement = () => ({
-  type: types.DECREMENT
+export const decrement = (index) => ({
+  type: types.DECREMENT,
+  index
 });
 
-export const setColor = (color) => ({
+export const setColor = (index, color) => ({
   type: types.SET_COLOR,
+  index,
   color
 });
