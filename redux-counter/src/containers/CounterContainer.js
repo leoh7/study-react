44dd39dcba9d3c4a88d62053f1fdf7ec94ced69b
leoh7 <leoh7@naver.com>
2019-01-20 21:29:43 +0900

    주석 추가

--- redux-counter/src/containers/CounterContainer.js
+++ redux-counter/src/containers/CounterContainer.js
@@ -2,6 +2,7 @@ import Counter from '../components/Counter';
 import * as actions from '../actions';
 import {connect} from 'react-redux';
 
+// 13가지 색상 중 랜덤으로 선택하는 함수
 export function getRandomColor() {
   const colors = [
     '#495057',
@@ -18,17 +19,20 @@ export function getRandomColor() {
     '#f59f00',
     '#f76707'
   ];
-
+  // 0부터 12까지 랜덤 숫자
   const random = Math.floor(Math.random() * 13);
-
+  // 랜덤한 색상 반환
   return colors[random];
 }
 
+// store 안의 state 값을 props로 연결한다
 const mapStateToProps = (state) => ({
   color: state.color,
   number: state.number
 })
 
+// 액션 생성 함수를 사용하여 액션을 생성하고,
+// 해당 액션을 dispatch하는 함수를 만든 후 이를 props로 연결한다
 const mapDispatchToProps = (dispatch) => ({
   onIncrement: () => dispatch(actions.increment()),
   onDecrement: () => dispatch(actions.decrement()),
@@ -38,6 +42,8 @@ const mapDispatchToProps = (dispatch) => ({
   }
 })
 
+// Counter 컴포넌트의 container 컴포넌트
+// Counter 컴포넌트를 애플리케이션의 데이터 레이어와 묶는 역할을 한다
 const CounterContainer = connect(
   mapStateToProps,
   mapDispatchToProps
