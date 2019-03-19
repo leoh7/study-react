93832e6b5943a393176c6474646c9233a7d15e2d
leoh7 <leoh7@naver.com>
2019-03-19 13:50:55 +0900

    isBlue 변수를 사용해서 동적인 스타일을 꾀할수도 있다

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import styles from './App.css';
+const cx = classNames.bind(styles);
 
 console.log(styles);
-const cx = classNames.bind(styles);
+
 class App extends Component {
   render() {
+		const isBlue = true;
+
     return (
-			<div className={cx('box', 'blue')}>
+			<div className={cx('box', {
+				blue: isBlue
+			})}>
 
 			</div>
     );
