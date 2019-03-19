462b6ec22800c46335aaf6a63406e04e9fba94f7
leoh7 <leoh7@naver.com>
2019-03-19 16:51:46 +0900

    이전 예제 내용 주석처리

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import styles from './App.scss';
-const cx = classNames.bind(styles);
-
-console.log(styles);
+// import classNames from 'classnames/bind';
+// import styles from './App.scss';
+// const cx = classNames.bind(styles);
+// console.log(styles);
 import Button from './components/Button';
 
 class App extends Component {
   render() {
-		const isBlue = true;
+		// const isBlue = true;
 
     return (
-			<div className={cx('box', {
-				blue: isBlue
-			})}>
-				<div className={cx('box-inside')}/>
+			// <div className={cx('box', {
+			// 	blue: isBlue
+			// })}>
+			// 	<div className={cx('box-inside')}/>
+			// </div>
 			<div>
 				<Button>버튼</Button>
 			</div>
