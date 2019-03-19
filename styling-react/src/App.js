5f088e452fab6f877da7bc391e126535f788b5e2
leoh7 <leoh7@naver.com>
2019-03-19 13:30:08 +0900

    classnames의 bind 기능을 사용하여 편의성 증진

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './App.css';
 
 console.log(styles);
+const cx = classNames.bind(styles);
 class App extends Component {
   render() {
     return (
-			<div className={classNames(styles.box, styles.blue)}>
+			<div className={cx('box', 'blue')}>
 
 			</div>
     );
