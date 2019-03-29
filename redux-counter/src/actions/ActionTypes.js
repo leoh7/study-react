82cb1b2978597b266230dfe2af4c12095c036e1a
leoh7 <leoh7@naver.com>
2019-03-29 11:10:30 +0900

    Action 종류들을 선언

---
+++ redux-counter/src/actions/ActionTypes.js
@@ -0,0 +1,7 @@
+// Action 종류들을 선언,
+// 앞에 export를 붙여 나중에 불러올 때
+// import * as types from './ActionTypes'를 할 수 있다
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
