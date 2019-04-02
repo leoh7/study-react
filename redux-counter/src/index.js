77212fcf24abfbf4af438b61bf6ce9e4f5448365
leoh7 <leoh7@naver.com>
2019-04-02 11:26:32 +0900

    [/src/] 엔트리 포인트 src/index.js 에 스토어 생성

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
+
+// 리덕스 관련 import
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
