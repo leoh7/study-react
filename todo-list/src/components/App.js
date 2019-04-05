52bf44399cf3e89de71acda61ffbd87516934b7e
leoh7 <leoh7@naver.com>
2019-04-06 01:20:22 +0900

    [[TODO] onInsert를 정의하여 App 이 TodoList로 todos를 넘기면서, 현존하는 todos 배열에 새 데이터를 집어넣게함

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -5,8 +5,18 @@ import TodoList from './TodoList';
 
 class App extends Component {
   state = {
-    input: ''
+    input: '',
+    todos: [
+      { id:0, text: '리액트 공부하기', done: true },
+      { id:1, text: '컴포넌트 스타일링 해보기', done: false }
+    ]
   }
+
+  id = 1;
+  getId = () => {
+    return ++this.id;
+  }
+
   handleChange = (e) => {
     const { value } = e.target;
     this.setState({
@@ -14,16 +24,30 @@ class App extends Component {
     });
   }
 
+  handleInsert = () => {
+    const { todos, input } = this.state;
+    const newTodo = {
+      id: this.getId(),
+      text: input,
+      done: false
+    }
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    })
+  }
+
   render() {
-    const { input } = this.state; // 비구조화 할당
+    const { input, todos } = this.state; // 비구조화 할당
     const {
-      handleChange
+      handleChange,
+      handleInsert
     } = this; // 비구조화 할당
 
     return(
       <PageTemplate>
-        <TodoInput onChange={handleChange} value={input} />
-        <TodoList />
+        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
+        <TodoList todos={todos} />
       </PageTemplate>
     )
   }
