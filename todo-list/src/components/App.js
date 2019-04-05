aa65141af6a1296dc342e1f2bcc870f9e908fffb
leoh7 <leoh7@naver.com>
2019-04-06 01:37:24 +0900

    [[TODO] onToggle을 정의, index를 찾아 done 값을 반전시킴

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -37,17 +37,41 @@ class App extends Component {
     })
   }
 
+  // todo 아이템 토글하기
+  handleToggle = (id) => {
+    // id로 배열의 인덱스를 찾음
+    const { todos } = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+
+    // 찾은 데이터의 done 값을 반전시킴
+    const toggled = {
+      ...todos[index],
+      done: !todos[index].done
+    }
+
+    // slice를 사용하여 찾은 index 전후의 데이터를 복사,
+    // 그 사이에는 변경된 todo 객체를 넣어줌
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    });
+  }
+
   render() {
     const { input, todos } = this.state; // 비구조화 할당
     const {
       handleChange,
-      handleInsert
+      handleInsert,
+      handleToggle
     } = this; // 비구조화 할당
 
     return(
       <PageTemplate>
         <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={handleToggle} />
       </PageTemplate>
     )
   }
