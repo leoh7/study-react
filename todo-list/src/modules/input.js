e1733055f1c96494e320f7b4ec383d90eb51c9e1
leoh7 <leoh7@naver.com>
2019-04-06 17:54:38 +0900

    [리덕스편하게] input 모듈 액션 타입 정의

--- todo-list/src/modules/input.js
+++ todo-list/src/modules/input.js
@@ -1,2 +1,5 @@
 import { Map } from 'immutable';
 import { handleActions, createActions } from 'redux-actions';import { handleActions, createActions } from 'redux-actions';
+// 액션 타입 정의
+// 리듀서 이름을 접두사로 설정함
+const SET_INPUT = 'input/SET_INPUT';
