fea7510150b376f6b44a220c78435a699ec85cb5
leoh7 <leoh7@naver.com>
2019-04-06 17:55:17 +0900

    커밋한 내용의 중복

--- todo-list/src/modules/input.js
+++ todo-list/src/modules/input.js
@@ -1,7 +1,9 @@
 import { Map } from 'immutable';
-import { handleActions, createActions } from 'redux-actions';import { handleActions, createActions } from 'redux-actions';
+import { handleActions, createActions } from 'redux-actions';
+
 // 액션 타입 정의
 // 리듀서 이름을 접두사로 설정함
-const SET_INPUT = 'input/SET_INPUT';// 액션 생성 함수
+const SET_INPUT = 'input/SET_INPUT';
+
 // 액션 생성 함수
 export const setInput = createActions(SET_INPUT);
