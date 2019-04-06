acad0d45c33ed382b0ba339e90a62c5307252164
leoh7 <leoh7@naver.com>
2019-04-06 18:05:15 +0900

    [리덕스편하게] input 모듈 리듀서 정의

--- todo-list/src/modules/input.js
+++ todo-list/src/modules/input.js
@@ -8,7 +8,15 @@ const SET_INPUT = 'input/SET_INPUT';
 // 액션 생성 함수
 export const setInput = createActions(SET_INPUT);
 
+
 // 리듀서 초기 상태 정의
 const initailState = Map({
   value: ''
 });
+
+// 리듀서
+export default handleActions({
+  [SET_INPUT]: (state, action) => {
+    return state.set('value', action.payload)
+  }
+}, initailState);
