1662b89e90f31af70e094f7faf6fc2cf71bf060a
leoh7 <leoh7@naver.com>
2019-04-07 14:52:21 +0900

    [리덕스편하게] TodoInputContainer 의 store 상태와 액션 생성 함수 연결

---
+++ todo-list/src/containers/TodoInputContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TodoInput from '../components/TodoInput';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as inputActions from '../modules/input';
+import * as todosActions from '../modules/todos';
+
+class TodoInputContainer extends Component {
+  render() {
+    return (
+      <TodoInput/>
+    );
+  }
+}
+
+export default connect(
+  (state) => ({
+    value: state.input.get('value')
+  }),
+  (dispatch) => ({
+    inputActions: bindActionCreators(inputActions, dispatch),
+    todosActions: bindActionCreators(todosActions, dispatch)
+  })
+)(TodoInputContainer);
