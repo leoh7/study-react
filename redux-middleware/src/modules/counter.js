7ed293b75bd2e3a859d40b020f69920cee0941c7
leoh7 <leoh7@naver.com>
2019-04-09 00:20:21 +0900

    [미들웨어] 모듈 counter에 작업전 주석처리

--- redux-middleware/src/modules/counter.js
+++ redux-middleware/src/modules/counter.js
@@ -1,11 +1,14 @@
 import { handleActions, createAction } from 'redux-actions';
 
+// 액션 타입 정의
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 
+// 액션 생성 함수
 export const increment = createAction(INCREMENT);
 export const decrement = createAction(DECREMENT);
 
+// 리덕스
 export default handleActions({
     [INCREMENT]: (state, action) => state + 1,
     [DECREMENT]: (state, action) => state - 1
