873f9adf714dd218d2b88de2d78717f023d5da89
leoh7 <leoh7@naver.com>
2019-04-09 16:34:52 +0900

    [미들웨어] 요청 완료 후에 .then과 .catch 를 활용하여 추가 작업 구현

--- redux-middleware/src/App.js
+++ redux-middleware/src/App.js
@@ -8,7 +8,12 @@ class App extends Component {
     
     loadData = () => {
         const { PostActions, number } = this.props;
-        PostActions.getPost(number);
+        PostActions.getPost(number)
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error);
+            });
     }
 
     componentDidMount() {
