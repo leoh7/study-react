8dfc2be9c6e82540e4bc3b41e5e7c3ac782d2790
leoh7 <leoh7@naver.com>
2019-04-09 16:35:37 +0900

    [미들웨어] 이전 작업을 ES7 중 async/await 문법 사용

--- redux-middleware/src/App.js
+++ redux-middleware/src/App.js
@@ -8,12 +8,12 @@ class App extends Component {
     
     loadData = () => {
         const { PostActions, number } = this.props;
-        PostActions.getPost(number)
-            .then((response) => {
-                console.log(response);
-            }).catch((error) => {
-                console.log(error);
-            });
+        try {
+            const response = await PostActions.getPost(number);
+            console.log(response);
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     componentDidMount() {
