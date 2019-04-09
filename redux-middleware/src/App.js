c9ef2b8db9b2d3e14b510ad4705bcc88433ac9dc
leoh7 <leoh7@naver.com>
2019-04-09 14:47:18 +0900

    [미들웨어] loadData 메서드를 만들어 렌더링함

--- redux-middleware/src/App.js
+++ redux-middleware/src/App.js
@@ -5,14 +5,42 @@ import * as counterActions from './modules/counter';
 import * as postActions from './modules/post';
 
 class App extends Component {
-    componentDidMount() {
+    
+    loadData = () => {
+        const { PostActions, number } = this.props;
+        PostActions.getPost(number);
     }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.number !== prevProps.number) {
+            this.loadData();
+        }
+    }
+
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, error, loading } = this.props;
         
         return (
             <div>
                 <h1>{number}</h1>
+                {
+                    loading
+                        ? (<h2>로딩중...</h2>)
+                        : (
+                            error
+                                ? (<h2>오류 발생!</h2>)
+                                : (
+                                    <div>
+                                        <h2>{post.title}</h2>
+                                        <p>{post.body}</p>
+                                    </div>
+                                )
+                        )
+                }
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
             </div>
