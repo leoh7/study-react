8b1a68eb077f88de839f2712ee29b7b9341a6182
leoh7 <leoh7@naver.com>
2019-04-09 09:56:20 +0900

    [미들웨어] thunk 생성함수 정의 / Async(비동기) 방식

--- redux-middleware/src/modules/counter.js
+++ redux-middleware/src/modules/counter.js
@@ -8,6 +8,23 @@ const DECREMENT = 'DECREMENT';
 export const increment = createAction(INCREMENT);
 export const decrement = createAction(DECREMENT);
 
+// thunk 생성함수
+export const incrementAsync = () => 
+    dispatch => {
+    // 1초 뒤 액션 디스패치
+    setTimeout(
+        () => { dispatch(increment()) },
+        10 * 1000
+    );
+}
+export const decrementAsync = () => 
+    dispatch => {
+    setTimeout(
+        () => { dispatch(decrement()) },
+        1 * 1000
+    );
+}
+
 // 리덕스
 export default handleActions({
     [INCREMENT]: (state, action) => state + 1,
