a849bcc1c16faa1147c32318f29e726d01c7b447
leoh7 <leoh7@naver.com>
2019-04-14 07:57:06 +0900

    [백엔드: Koa 프레임워크] app.use 를 사용한 미들웨어 등록 미들웨어는 app.use 로 등록하는 순서대로 처리한다. 이 때 next를 호출하지 않으면 나머지는 무시하게 됨. next() 는 Promise를 반환하는데, .then()을 사용하여 다음 작업들이 끝나고 난 후 특정 작업을 수행 할 수도 있다.

---
+++ blog/blog-backend/src/index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  console.log(1);
+  next().then(() => {
+    console.log('bye');
+  });
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+  console.log(ctx);
+});
+
+app.listen(4000, () => {
+  console.log('listening to port 4000');
+});
