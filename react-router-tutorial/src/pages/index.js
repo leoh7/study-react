2a161b25e369c11ae93bed6e7fd748bc4bd1456b
leoh7 <leoh7@naver.com>
2019-04-13 14:29:45 +0900

    [react-router SPA] 라우트 안의 라우트 - Post 페이지 컴포넌트 생성 및 index 등록

--- react-router-tutorial/src/pages/index.js
+++ react-router-tutorial/src/pages/index.js
@@ -1,4 +1,5 @@
 // 다음 코드는 컴포넌트를 불러온 후 동일한 이름으로 내보낸다
 
 export { default as Home } from './Home';
-export { default as About } from './About'
+export { default as About } from './About';
+export { default as Post } from './Post';
