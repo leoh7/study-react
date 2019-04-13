9b48a418f1255d165a328a9f1c315c164741a485
leoh7 <leoh7@naver.com>
2019-04-13 15:22:03 +0900

    [react-router SPA] 라우트 안의 라우트 - Posts 페이지 컴포넌트 생성 및 index 등록, 메뉴에 추가

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -3,7 +3,8 @@ import { Route } from 'react-router-dom'
 
 import {
   Home,
-  About
+  About,
+  Posts
 } from 'pages';
 import Menu from './components/Menu'
 
@@ -15,6 +16,7 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         {/* <Route exact path="/about" component={About} /> */}
         <Route path="/about/:name?" component={About} />
+        <Route path="/posts" component={Posts} />
       </div>
     );
   }
