f23b7863cddb80b3442936cc51a877e542962dd8
leoh7 <leoh7@naver.com>
2019-04-19 20:17:44 +0900

    [백엔드: Koa 프레임워크] posts 컨트롤러 작성 / remove (특정 포스트 제거)

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -49,6 +49,19 @@ exports.read = (ctx) => {
 
 // 포스트 제거
 // DELETE /api/posts/:id
+exports.remove = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex(p => p.id.toString() === id);
+  if(index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다'
+    };
+    return;
+  }
+  posts.splice(index, 1);
+  ctx.status = 204; // No Content
+}
 
 // 포스트 수정(교체)
 // PUT /api/posts/:id
