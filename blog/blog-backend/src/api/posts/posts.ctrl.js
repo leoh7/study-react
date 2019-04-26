c5b65d1e5b0faa03eaa50fbbd42413c35eba7333
leoh7 <leoh7@naver.com>
2019-04-26 22:28:41 +0900

    [mongoose-pagination] SET Limit for post body

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -34,11 +34,16 @@ exports.list = async (ctx) => {
       .sort({_id: -1})  // 내림차순 정렬 -1 / 오름차순 정렬 1
       .limit(10)
       .skip((page - 1) * 10)
+      .lean()
       .exec();
       
     const postCount = await Post.countDocuments().exec();
+    const limitBodyLength = post => ({
+      ...post,
+      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+    });
+    ctx.body = posts.map(limitBodyLength);
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts;
   } catch (e) {
     ctx.throw(e, 500);    
   }
