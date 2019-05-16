9e61e02dd6fdc57a5abcb34776ea0a92fc96dd64
leoh7 <leoh7@naver.com>
2019-05-17 00:12:51 +0900

    [blog-front] 스토어 생성함수 configure 구현

--- blog/blog-frontend/src/Root.js
+++ blog/blog-frontend/src/Root.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
+import configure from 'store/configure';
+
+const store = configure();
 
 const Root = () => {
   return (
