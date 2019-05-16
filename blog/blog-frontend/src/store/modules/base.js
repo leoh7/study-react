d6c3807ce9b40ab21e50099fb64d0855969e02b9
leoh7 <leoh7@naver.com>
2019-05-16 23:08:45 +0900

    [blog-front] Add Redux modules / base, editor, list, post

---
+++ blog/blog-frontend/src/store/modules/base.js
@@ -0,0 +1,17 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+// 로그인 상태, 삭제 및 로그인할 때 보이는 모달 상태
+
+// action types
+
+// action creators
+
+// initial state
+const initialState = Map({});
+
+// reducer
+export default handleActions({
+
+}, initialState);
---
+++ blog/blog-frontend/src/store/modules/editor.js
@@ -0,0 +1,17 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+// 마크다운 에디터 상태
+
+// action types
+
+// action creators
+
+// initial state
+const initialState = Map({});
+
+// reducer
+export default handleActions({
+
+}, initialState);
