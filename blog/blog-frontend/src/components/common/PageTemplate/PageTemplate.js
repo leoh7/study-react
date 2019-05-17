17d4c0c63e677c085145cefb380bc539da4ba421
leoh7 <leoh7@naver.com>
2019-05-17 22:45:19 +0900

    [blog-front] PageTemplate 에 결합 및 렌더링

--- blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => {
   return (
     <div className={cx('page-template')}>
-      PageTemplate
+      <Header />
+      <Footer />
     </div>
   );
 }
