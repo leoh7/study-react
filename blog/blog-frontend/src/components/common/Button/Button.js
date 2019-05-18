82b1da75e5dca4acca9f152469e729d0544254c9
leoh7 <leoh7@naver.com>
2019-05-19 00:10:51 +0900

    [blog-front] 버튼 임시 구현 및 임시 렌더링

---
+++ blog/blog-frontend/src/components/common/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Button.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const Button = () => {
+  return (
+    <div>
+      Button
+    </div>
+  );
+}
+
+export default Button;
--- blog/blog-frontend/src/pages/ListPage.js
+++ blog/blog-frontend/src/pages/ListPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
+import Button from 'components/common/Button';
 
 const ListPage = () => {
   return (
     <PageTemplate>
       List
+      <Button />
     </PageTemplate>
   );
 };
