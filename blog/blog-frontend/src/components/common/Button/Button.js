df8452fd8a87cf478c77a4f7dde6fd31a5ab97f5
leoh7 <leoh7@naver.com>
2019-05-20 19:44:17 +0900

    [blog-front] 버튼구현 마무리

--- blog/blog-frontend/src/components/common/Button/Button.js
+++ blog/blog-frontend/src/components/common/Button/Button.js
@@ -5,11 +5,24 @@ import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const Button = () => {
+const Div = ({children, ...rest}) => <div {...rest}>{children}</div>
+
+const Button = ({
+  children, to, onClick, disabled, theme = 'default',
+}) => {
+  // to 값이 존재하면 Link를 사용, 그렇지 않으면 div 사용
+  // 비활성화 되어 있는 버튼일 때도 div 사용
+  const Element = (to && !disabled) ? Link : Div;
+
+  // 비활성화 되면 onClick은 실행되지 않음
+  // disabled 값이 true 가 되면 className에 disabled 를 추가
   return (
-    <div>
-      Button
-    </div>
+    <Element
+      to={to}
+      className={cx('button', theme, {disabled})}
+      onClick={disabled ? () => null : onClick}>
+      {children}
+    </Element>
   );
 }
 
