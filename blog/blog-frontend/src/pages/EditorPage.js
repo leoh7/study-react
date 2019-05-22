443f7964c46ec9d851687aa47f60a1268d8dd879
leoh7 <leoh7@naver.com>
2019-05-23 01:04:56 +0900

    [blog-front] EditorPage에 EditorHeader 추가

--- blog/blog-frontend/src/pages/EditorPage.js
+++ blog/blog-frontend/src/pages/EditorPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
+import EditorHeader from 'components/editor/EditorHeader';
 
 const EditorPage = () => {
   return (
     <EditorTemplate
+      header={<EditorHeader/>}
     />
   );
 };
