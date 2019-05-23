1f1edcf9b5a0ff0b91c615ea6fe73c25feee29d5
leoh7 <leoh7@naver.com>
2019-05-23 18:15:48 +0900

    [blog-front] 에디터 모듈 추가

--- blog/blog-frontend/src/store/modules/editor.js
+++ blog/blog-frontend/src/store/modules/editor.js
@@ -5,13 +5,25 @@ import { pender } from 'redux-pender';
 // 마크다운 에디터 상태
 
 // action types
+const INITIALIZE = 'editor/INITIALIZE';
+const CHANGE_INPUT = 'editor/CHANGE_INPUT';
 
 // action creators
+export const initialize = createAction(INITIALIZE);
+export const changeInput = createAction(CHANGE_INPUT);
 
 // initial state
-const initialState = Map({});
+const initialState = Map({
+  title: '',
+  markdown: '',
+  tags: ''
+});
 
 // reducer
 export default handleActions({
-
+  [INITIALIZE]: (state, action) => initialState,
+  [CHANGE_INPUT]: (state, action) => {
+    const { name, value } = action.payload;
+    return state.set(name, value);
+  }
 }, initialState);
